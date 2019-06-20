2cc2222e58059095d66a237cba931bb78e180d29
anishdd77 <51217167+anishdd77@users.noreply.github.com>
2019-06-20 17:43:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,20 +7,20 @@ let mtickets   = true;
 let tchannels  = [];
 let current    = 0;
 client.on('ready', function(){
-    var ms = 10000;
+    var ms = 100000;
     var setGame = [`[ -new (تكت) ]`];   
     var i = -1;
     var j = 0;
     setInterval(function (){
         if( i == 1 ){
-            j = 1;
+            j = -1;
         }
         if( i == (setGame.length)-1 ){
             j = 1;
         }
         i = i+j;
         client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
-    }, ms);10000
+    }, ms);100000
 });
 
 client.on("message", async message => {
